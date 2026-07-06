37627352cffbc30779c4f959855233210c584ebf
Shashank <kvssshashank@gmail.com>
2026-07-06 13:37:32 +0530

    Create DoctorRepository.java

---
+++ DoctorRepository.java
@@ -0,0 +1,12 @@
+package com.project.back_end.repo;
+
+import com.project.back_end.models.Doctor;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface DoctorRepository extends JpaRepository<Doctor, Long> {
+    List<Doctor> findBySpecialty(String specialty);
+}

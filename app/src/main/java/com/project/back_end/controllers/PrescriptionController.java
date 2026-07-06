158e3ee4b42ac67bbf66b0e1038dc5207159362c
Shashank <kvssshashank@gmail.com>
2026-07-06 13:21:22 +0530

    Update PrescriptionController.java

--- app/src/main/java/com/project/back_end/controllers/PrescriptionController.java
+++ app/src/main/java/com/project/back_end/controllers/PrescriptionController.java
@@ -1,13 +1,19 @@
 package com.project.back_end.controllers;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/prescriptions")
 public class PrescriptionController {
 
-    @PostMapping("/create")
-    public String createPrescription(@RequestBody String prescriptionDetails) {
-        return "Prescription saved successfully";
+    @PostMapping
+    public ResponseEntity<String> issuePrescription(@RequestBody Object prescriptionData) {
+        return ResponseEntity.ok("Prescription successfully issued.");
+    }
+
+    @GetMapping("/{patientId}")
+    public ResponseEntity<Object> getPrescriptions(@PathVariable Long patientId) {
+        return ResponseEntity.ok().build();
     }
 }

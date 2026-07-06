00a95f8fbe27bf212c83906579b0bb40f8f4a893
Shashank <kvssshashank@gmail.com>
2026-07-06 13:21:42 +0530

    Update PatientRepository.java

--- app/src/main/java/com/project/back_end/repo/PatientRepository.java
+++ app/src/main/java/com/project/back_end/repo/PatientRepository.java
@@ -2,10 +2,8 @@ package com.project.back_end.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-// Assuming a Patient model exists similar to Doctor
-// import com.project.back_end.models.Patient; 
 
 @Repository
 public interface PatientRepository extends JpaRepository<Object, Long> {
-    // Custom database queries for Patients can be defined here
+    // Standard CRUD operations are automatically provided by JpaRepository
 }

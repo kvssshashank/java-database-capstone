b2efb778e3546d682d532bfb30e83846d380fb3c
Shashank <kvssshashank@gmail.com>
2026-07-06 13:22:23 +0530

    Update DoctorService.java

--- app/src/main/java/com/project/back_end/services/DoctorService.java
+++ app/src/main/java/com/project/back_end/services/DoctorService.java
@@ -2,19 +2,21 @@ package com.project.back_end.services;
 
 import com.project.back_end.models.Doctor;
 import org.springframework.stereotype.Service;
-import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 @Service
 public class DoctorService {
 
     public List<Doctor> getAllDoctors() {
-        // Logic to fetch all doctors from a repository goes here
-        return new ArrayList<>();
+        return Collections.emptyList();
     }
 
     public Doctor addDoctor(Doctor doctor) {
-        // Logic to save to database goes here
         return doctor;
+    }
+    
+    public Doctor getDoctorById(Long id) {
+        return new Doctor();
     }
 }
